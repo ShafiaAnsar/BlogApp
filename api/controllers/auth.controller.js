4fbeafe0d96529718c911fd7b45646a651b81eb3
shafiaansar <shafiaansar142@gmail.com>
2024-03-10 14:42:09 +0500

    Auth Routes and error middleware

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from '../modal/user.modal.js'
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
+export const signup = async (req,res,next)=>{
+    const {username, email,password } = req.body
+
+    if(!email || !password || !username || username === '' || password === '' || email === ''){
+        next(errorHandler(400,'All fields are required'))
+    }
+    const hashedPassword = bcryptjs.hashSync(password,10)
+
+    const newUser = new User(
+        {
+            username, email, password:hashedPassword 
+        }
+    )
+    try{
+    await newUser.save()
+    res.json({message:"Success"})
+    }
+    catch(err){
+        next(err)        
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB')
@@ -9,7 +10,21 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log(err)
 })
 const app = express()
+app.use(express.json())
 const port = 3000
 app.listen (port, () => {
     console.log(`Server running on port ${port}`)
+})
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Something went wrong'
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
 })

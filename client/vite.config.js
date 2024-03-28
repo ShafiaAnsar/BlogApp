34b50d62ded049da4725f2a77450dff65d6162e4
Shafia Ansar <shafiaansar142@gmail.com>
2024-03-28 13:37:49 +0500

    backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://blog-app-api-kappa.vercel.app',
         secure:false
       },
     },
